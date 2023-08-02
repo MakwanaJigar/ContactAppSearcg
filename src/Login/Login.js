9cc876e5b0beb61dd20b3b1c8854a5b7e3d1ef44
Makwana Jigar <jigar84252@gmail.com>
2023-08-02 13:50:22 +0530

    all changes

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -59,7 +59,49 @@ const validate=() => {
 }
 
   return (
-   <div className="row mt-5" >
+
+<>
+      {/*  */}
+
+      <section className="vh-100">
+  <div className="container py-5 h-100">
+    <div className="row d-flex align-items-center justify-content-center h-100">
+      <div className="col-md-8 col-lg-7 col-xl-6">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          className="img-fluid" alt="Phone image"/>
+      </div>
+      <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        <h1 className="fw-bold mb-3">Login</h1>
+        <form onSubmit={ProceedLogin}>
+          {/* <!-- Email input --> */}
+          <div className="form-outline mb-4">
+            <input type="text" id="form1Example13" className="form-control form-control-lg" value={username} onChange={e =>usernameupdate(e.target.value)} name="username" required/>
+            <label className="form-label" htmlFor="form1Example13">Email address</label>
+          </div>
+
+          {/* <!-- Password input --> */}
+          <div className="form-outline mb-4">
+            <input type="password" id="form1Example23" className="form-control form-control-lg" value={password} onChange={e =>passwordupdate(e.target.value)} name="password" required/>
+            <label className="form-label" htmlFor="form1Example23">Password</label>
+          </div>
+
+        
+
+          {/* <!-- Submit button --> */}
+          <button type="submit" className="btn btn-primary btn-lg btn-block">Login</button>
+          <Link className="btn btn-dark btn-lg btn-block mx-3" to='/createuser'>Create User</Link>
+
+          
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+      {/*  */}
+
+   {/* <div className="row mt-5" >
     <div className="offset-lg-3 col-lg-6">
       <form onSubmit={ProceedLogin}>
         <div className="card">
@@ -69,12 +111,12 @@ const validate=() => {
           <div className="card-body">
             <div className="form-group">
                   <label className="fw-bold">USER NAME</label>
-                  <input className="form-control" value={username} onChange={e =>usernameupdate(e.target.value)} type="text" placeholder="User Name" name="username" required/>
+                  <input className="form-control"  type="text" placeholder="User Name" value={username} onChange={e =>usernameupdate(e.target.value)} name="username" required/>
             </div>
 
             <div className="form-group">
                   <label className="fw-bold">PASSWORD</label>
-                  <input className="form-control" value={password} onChange={e =>passwordupdate(e.target.value)} type="password" placeholder="Password" name="password" required/>
+                  <input className="form-control"  type="password" placeholder="Password" value={password} onChange={e =>passwordupdate(e.target.value)} name="password" required/>
             </div>
 
           </div>
@@ -88,7 +130,8 @@ const validate=() => {
 
       </form>
     </div>
-   </div>
+   </div> */}
+   </>
   );
 };
 
--- src/Login/Registration.js
+++ src/Login/Registration.js
@@ -72,7 +72,96 @@ const Registration = () => {
     }
 
   return (
-    <div>
+    <>
+
+    {/*  */}
+
+    {/* <!-- Section: Design Block --> */}
+<section className="">
+  {/* <!-- Jumbotron --> */}
+  <div className="px-4 py-5 px-md-5 text-center text-lg-start" style={{backgroundColor:' hsl(0, 0%, 96%)'}}>
+    <div className="container">
+      <div className="row gx-lg-5 align-items-center">
+        <div className="col-lg-6 mb-5 mb-lg-0">
+          <h1 className="my-5 display-3 fw-bold ls-tight">
+            The best App <br />
+            <span className="text-primary">for your Contacts</span>
+          </h1>
+        
+        </div>
+
+        <div className="col-lg-6 mb-5 mb-lg-0">
+          <div className="card">
+            <div className="card-body py-5 px-md-5">
+                <h1 className='mb-3 fw-bold'>Create Account</h1>
+              <form onSubmit={handlesubmit}>  
+                {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
+                <div className="row">
+                  <div className="col-md-6 mb-4">
+                    <div className="form-outline">
+                      <input  id="form3Example1" className="form-control" required value={id} onChange={e => {idchange(e.target.value)}} type='text' name='username'/>
+                      <label className="form-label" htmlFor="form3Example1">User name</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6 mb-4">
+                    <div className="form-outline">
+                      <input type="text" id="form3Example2" className="form-control" value={name} onChange={e => {namechange(e.target.value)}} name='fullname' />
+                      <label className="form-label" htmlFor="form3Example2">Full name</label>
+                    </div>
+                  </div>
+                </div>
+
+                {/* <!-- Email input --> */}
+                <div className="form-outline mb-4">
+                  <input type="email" id="form3Example3" className="form-control" required value={email} onChange={e => {emailchange(e.target.value)}}  name='email' />
+                  <label className="form-label" htmlFor="form3Example3">Email </label>
+                </div>
+
+                {/* <!-- Password input --> */}
+                <div className="form-outline mb-4">
+                  <input id="form3Example4" className="form-control" required type='password' minLength={8} value={password} onChange={e => {passwordchange(e.target.value)}} name='password' />
+                  <label className="form-label" htmlFor="form3Example4">Password</label>
+                </div>
+
+
+                {/* <!-- Submit button --> */}
+                <button type="submit" className="btn btn-primary btn-block mb-4">
+                 Create
+                </button>
+                <Link className="btn btn-dark mx-3 btn-block mb-4" to='/'>Cancel</Link>
+
+                {/* <!-- Register buttons --> */}
+                {/* <div className="text-center">
+                  <p>or sign up with:</p>
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-google"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-twitter"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-github"></i>
+                  </button>
+                </div> */}
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* <!-- Jumbotron --> */}
+</section>
+{/* <!-- Section: Design Block --> */}
+
+    {/*  */}
+    {/* <div>
         
         <div className='offset-lg-3 col-lg-6'>
             <form className='container' onSubmit={handlesubmit}>
@@ -124,7 +213,8 @@ const Registration = () => {
 
         </div>
 
-    </div>
+    </div> */}
+    </>
   );
 }
 
